005df3652ca20fab286ee6278f5bc06808bf689b
Donny Roufs <iamchets@yahoo.com>
2023-06-10 13:37:47 +0200

    refactor: pomodoro

--- Pomodoro.Core/Pomodoro.cs
+++ Pomodoro.Core/Pomodoro.cs
@@ -4,7 +4,7 @@ public class Pomodoro
 {
     private TimeSpan _time = TimeSpan.FromMinutes(25);
     private PomodoroState _state = PomodoroState.Pending;
-    private int _cycles = 0;
+    private int _cycles;
     private readonly ITimer _timer;
 
     public Pomodoro(ITimer timer)
@@ -20,39 +20,51 @@ public class Pomodoro
     public void Start()
     {
         _state = PomodoroState.Work;
-
-        _timer.Tick(() =>
-        {
-            _time = _time.Subtract(TimeSpan.FromSeconds(1));
-
-            if (_time.Equals(TimeSpan.Zero))
-            {
-                if (_state == PomodoroState.Work)
-                {
-                    _cycles += 1;
-
-                    if (_cycles % 3 == 0)
-                    {
-                        _state = PomodoroState.LongBreak;
-                        _time = TimeSpan.FromMinutes(20);
-                        return;
-                    }
-
-                    _state = PomodoroState.ShortBreak;
-                    _time = TimeSpan.FromMinutes(5);
-                }
-                else
-                {
-                    _state = PomodoroState.Work;
-                    _time = TimeSpan.FromMinutes(25);
-                }
-            }
-        });
+        _timer.Tick(OnTick);
     }
 
     public PomodoroState GetState()
     {
         return _state;
+    }
+
+    private void OnTick()
+    {
+        _time = _time.Subtract(TimeSpan.FromSeconds(1));
+
+        if (!_time.Equals(TimeSpan.Zero)) return;
+
+        if (_state == PomodoroState.Work)
+        {
+            HandleWorkState();
+            return;
+        }
+
+        HandleBreakState();
+    }
+
+    private void HandleWorkState()
+    {
+        _cycles += 1;
+
+        if (_cycles % 3 == 0)
+        {
+            SetState(PomodoroState.LongBreak, TimeSpan.FromMinutes(20));
+            return;
+        }
+
+        SetState(PomodoroState.ShortBreak, TimeSpan.FromMinutes(5));
+    }
+
+    private void HandleBreakState()
+    {
+        SetState(PomodoroState.Work, TimeSpan.FromMinutes(25));
+    }
+
+    private void SetState(PomodoroState state, TimeSpan time)
+    {
+        _state = state;
+        _time = time;
     }
 }
 
