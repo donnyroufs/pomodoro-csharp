0890029e0fd4b07f054838b99ce4ff7d6df5561b
Donny Roufs <iamchets@yahoo.com>
2023-06-10 15:30:48 +0200

    feat: hook up CLI

---
+++ Pomodori.CLI/Program.cs
@@ -0,0 +1,16 @@
+﻿using Pomodori.CLI;
+using Pomodori.Core;
+
+var timer = new TimerImpl();
+var pomodoro = new Pomodoro(timer);
+
+pomodoro.Start();
+
+var viewModel = new ViewModel(pomodoro);
+
+while (true)
+{
+    Console.CursorVisible = false;
+    Console.Write(viewModel);
+    Console.SetCursorPosition(0, 0);
+}
