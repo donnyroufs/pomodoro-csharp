124b2c00bf4e13bb66db816aea23dc593238d6b0
Donny Roufs <iamchets@yahoo.com>
2023-06-10 12:55:05 +0200

    feat: introduce pomodoro state

--- Pomodoro.Core.UnitTests/PomodoroTests.cs
+++ Pomodoro.Core.UnitTests/PomodoroTests.cs
@@ -6,25 +6,76 @@ namespace Pomodoro.Core.UnitTests;
 public class PomodoroShould
 {
     [Test]
+    public void BeInPendingStateWhenNotStarted()
+    {
+        var timer = new FakeTimer();
+        Pomodoro pomodoro = new(timer);
+
+
+        pomodoro
+            .GetState()
+            .Should()
+            .Be(PomodoroState.Pending);
+    }
+
+    [Test]
+    public void BeInWorkStateWhenStarted()
+    {
+        var timer = new FakeTimer();
+        Pomodoro pomodoro = new(timer);
+
+        pomodoro.Start();
+        
+        pomodoro
+            .GetState()
+            .Should()
+            .Be(PomodoroState.Work);
+    }
+
+    [Test]
     public void DecrementTheRemainingTimeEveryClockTick()
     {
         var timer = new FakeTimer();
         Pomodoro pomodoro = new(timer);
-        
+
         pomodoro.Start();
-        
+
         timer.SimulateClockTick();
 
         pomodoro
             .GetRemainingTime()
             .Should()
             .Be(1499);
-        
+
         timer.SimulateClockTick();
-        
+
         pomodoro
             .GetRemainingTime()
             .Should()
             .Be(1498);
+    }
+
+    [Test]
+    public void SwitchFromWorkToShortBreak()
+    {
+        var timer = new FakeTimer();
+        Pomodoro pomodoro = new(timer);
+
+        pomodoro.Start();
+
+        Enumerable
+            .Range(0, 1500)
+            .ToList()
+            .ForEach(_ => { timer.SimulateClockTick(); });
+
+        pomodoro
+            .GetRemainingTime()
+            .Should()
+            .Be(300);
+
+        pomodoro
+            .GetState()
+            .Should()
+            .Be(PomodoroState.ShortBreak);
     }
 }
--- Pomodoro.Core/Pomodoro.cs
+++ Pomodoro.Core/Pomodoro.cs
@@ -3,6 +3,7 @@
 public class Pomodoro
 {
     private TimeSpan _time = TimeSpan.FromMinutes(25);
+    private PomodoroState _state = PomodoroState.Pending;
     private readonly ITimer _timer;
 
     public Pomodoro(ITimer timer)
@@ -17,9 +18,36 @@ public class Pomodoro
 
     public void Start()
     {
+        _state = PomodoroState.Work;
+        
         _timer.Tick(() =>
         {
             _time = _time.Subtract(TimeSpan.FromSeconds(1));
+
+            if (_time.Equals(TimeSpan.Zero))
+            {
+                if (_state == PomodoroState.Work)
+                {
+                    _state = PomodoroState.ShortBreak;
+                    _time = TimeSpan.FromMinutes(5);
+                }
+                else
+                {
+                    _state = PomodoroState.Work;
+                }
+            }
         });
     }
+
+    public PomodoroState GetState()
+    {
+        return _state;
+    }
+}
+
+public enum PomodoroState
+{
+    ShortBreak,
+    Pending,
+    Work
 }
